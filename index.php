aafc137869ab0373603c6ee12266d5105d2eeb1b
Apo <afuthexal@gmail.com>
2017-08-10 21:39:14 +0500

    added meta and facebook recommend button

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CODEFISH</title>
+	<meta name="description" content="CODEFISH - A multidiciplinary design and development studio in maldives"/>
+	<meta name="keywords" content="Maldives Web development, Web Design, App Develpoment, App Design, Creative Studio, Maldives" />
+
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="CodeFish" />
+	<meta property="og:description" content="CODEFISH - A multidiciplinary design and development studio in maldives" />
+	<meta property="og:url" content="http://codefish.mv"/>
+	<meta property="og:site_name" content="CODEFISH" />
+	<meta property="og:image" content="img/og.png" />
+
+	<meta name="twitter:card" content="summary"/>
+	<meta name="twitter:site" content="@codefishmv"/>
+	<meta name="twitter:title" content="CODEFISH"/>
+	<meta name="twitter:description" content="CODEFISH - A multidiciplinary design and development studio in maldives"/>
+	<meta name="twitter:creator" content="@codefishmv"/>
+	<meta name="twitter:image:src" content="img/og.png"/>
+
 	<title>Codefish</title>
 	<link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css?v=23">
@@ -12,9 +33,12 @@
 			<svg width="200" height="150" viewBox="0 0 200 150" xmlns="http://www.w3.org/2000/svg"><g fill="#00C4D6" fill-rule="evenodd"><rect x="57.364" width="27.907" height="148.819" rx="13.953"/><path d="M26.796 0c-8.563 0-15.17 1.8-19.82 5.4C2.326 8.997 0 14.002 0 20.412V128.893c0 6.41 2.325 11.414 6.976 15.013 4.65 3.6 11.257 5.4 19.82 5.4V0zM114.73 0c8.562 0 15.168 1.8 19.82 5.4 4.65 3.598 6.975 8.603 6.975 15.013V128.893c0 6.41-2.326 11.414-6.976 15.013-4.652 3.6-11.258 5.4-19.82 5.4V0zM193.906 80.895c-3.47-3.43-7.566-5.54-12.29-6.327 4.724-.787 8.82-2.868 12.29-6.242 3.47-3.374 5.204-7.592 5.204-12.653V23.28c0-2.585-.22-23.28-.22-23.28-8.564 0-15.17 1.8-19.82 5.4-4.652 3.598-6.977 8.603-6.977 15.013V128.893c0 6.41 2.325 11.414 6.976 15.013 4.65 3.6 11.256 5.4 19.82 5.4l.22-23.282V93.632c0-5.06-1.734-9.307-5.204-12.737z"/></g></svg>
 			<div class="logo-text">CODEFISH - MULTIDICIPLINARY DESIGN STUDIO IN MALDIVES</div>
 
-			<a href="#" class="quote-btn">
+			<a href="mailto:info@codefish.mv" class="quote-btn">
 				GET A QUOTE
 			</a>
+
+			<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fcodefish.mv&width=140&layout=button&action=recommend&size=small&show_faces=false&share=true&height=65&appId=185466921505058" width="140" height="65" style="margin-top: 40px;border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+
 		</div>
 
 	</div>
